96722cbbe57d9fb933b8d2342106f6dda8d9564c
Random32I <104802424+Random32I@users.noreply.github.com>
2023-02-08 15:00:57 -0500

    Added a bunch of stuff
    added particle effects, interactable objects, post processing, render texture of a camera watching the main area, local light source

--- 3D thing/Assets/Scripts/Player.cs
+++ 3D thing/Assets/Scripts/Player.cs
@@ -10,6 +10,8 @@ public class Player : MonoBehaviour
     [SerializeField] int jumpForce;
     [SerializeField] int maxSpeed;
     [SerializeField] int speed;
+    Rigidbody boxRig;
+    bool holding;
 
     [SerializeField] Vector2 sensitivity;
     [SerializeField] Vector2 rotation;
@@ -32,6 +34,8 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //Mouse Movement
         Vector2 wantedVelocity = GetInput() * sensitivity;
 
         if (rotation.y < 90 && rotation.y > -90)
@@ -51,28 +55,59 @@ public class Player : MonoBehaviour
             rotation.y = -89.99f;
         }
 
-        //transform.Rotate(new Vector3(0, Input.GetAxisRaw("Horizontal") / 3, 0));
-
+        //Jump
         if (Input.GetKeyDown(KeyCode.Space) && rig.velocity.y == 0)
         {
             rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+        //Interact
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (!holding)
+            {
+                RaycastHit hit;
+                int layerMask = 1 << 8;
+
+                if (Physics.Raycast(transform.position, GameObject.Find("Main Camera").transform.forward, out hit, 5, layerMask))
+                {
+                    holding = true;
+                    boxRig = hit.rigidbody;
+
+                    boxRig.gameObject.transform.position = transform.position + GameObject.Find("Main Camera").transform.forward * 2 + Vector3.up /2;
+                    boxRig.useGravity = false;
+                    boxRig.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+                    boxRig.velocity = Vector3.zero;
+                }
+            }
+            else
+            {
+                holding = false;
+                boxRig.useGravity = true;
+                boxRig.constraints = RigidbodyConstraints.None;
+                boxRig = null;
+            }
+        }
+        if (boxRig)
+        {
+            boxRig.gameObject.transform.position = transform.position + GameObject.Find("Main Camera").transform.forward * 2 + Vector3.up / 2;
+            boxRig.rotation = Quaternion.Euler(new Vector3(0, GameObject.Find("Main Camera").transform.eulerAngles.y, 0));
+        }
+
+        //Debug
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            Debug.Log(new Vector3(GameObject.Find("Main Camera").transform.forward.x * Input.GetAxisRaw("Vertical") * speed, rig.velocity.y, GameObject.Find("Main Camera").transform.forward.z * Input.GetAxisRaw("Horizontal") * speed));
+            Debug.Log(GameObject.Find("Main Camera").transform.forward);
+            Debug.Log(new Vector2 (Input.GetAxisRaw("Vertical"), Input.GetAxisRaw("Horizontal")));
         }
     }
 
     private void FixedUpdate()
     {
+        //Movement (probly a better way, but it works)
         rig.velocity = new Vector3(((float)Math.Sin(GameObject.Find("Main Camera").transform.rotation.eulerAngles.y * Math.PI / 180) * Input.GetAxisRaw("Vertical") * speed) + ((float)Math.Cos(GameObject.Find("Main Camera").transform.rotation.eulerAngles.y * Math.PI / 180) * Input.GetAxisRaw("Horizontal") * speed), rig.velocity.y, 
             ((float)Math.Cos(GameObject.Find("Main Camera").transform.rotation.eulerAngles.y * Math.PI / 180) * Input.GetAxisRaw("Vertical") * speed) - ((float)Math.Sin(GameObject.Find("Main Camera").transform.rotation.eulerAngles.y * Math.PI / 180) * Input.GetAxisRaw("Horizontal") * speed));
-
-        //rig.MovePosition(rig.position + movement * speed * Time.deltaTime);
-        
-        /*if (rig.velocity.x >= maxSpeed)
-        {
-            rig.velocity = new Vector3(maxSpeed, rig.velocity.y, rig.velocity.z);
-        }
-        else if (rig.velocity.z >= maxSpeed)
-        {
-            rig.velocity = new Vector3(rig.velocity.x, rig.velocity.y, maxSpeed);
-        }*/
+        //rig.velocity = new Vector3(GameObject.Find("Main Camera").transform.forward.x * Input.GetAxisRaw("Horizontal") * speed - GameObject.Find("Main Camera").transform.forward.x * Input.GetAxisRaw("Vertical") * speed, rig.velocity.y, GameObject.Find("Main Camera").transform.forward.z * Input.GetAxisRaw("Vertical") * speed - GameObject.Find("Main Camera").transform.forward.z * Input.GetAxisRaw("Horizontal") * speed);
     }
 }
